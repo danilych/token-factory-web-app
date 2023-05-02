6112bf717c07b6bb1a065eb2417fea38cec181d9
Danilych <danilprov2004@gmail.com>
2023-05-02 17:30:53 +0200

    feat: connect wallet

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,49 @@
 import { Button, Label, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
-
+import { ethers } from "ethers";
 
 export default function Home() {
   // const [isConnected, setIsConnected] = useState(false);
+  const [account, setAccount] = useState("0x0000");
+
+  const [isConnected, setIsConnected] = useState(false);
+
+  let provider: any;
+
+  const [signer, setSigner] = useState();
+
+  async function connectWallet() {
+    if (window.ethereum == null) {
+      console.log("MetaMask not installed");
+    } else {
+      try {
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        const address = await provider.send("eth_requestAccounts", []);
+
+        setAccount(address[0]);
+
+        setSigner(provider.getSigner());
+
+        setIsConnected(true);
+      } catch (error) {
+        console.log("Error connection...");
+
+        console.log(error);
+      }
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-8">
       <div className="absolute top-6 right-6">
-        <Button outline={true} gradientDuoTone="purpleToBlue">
-          Connect wallet
+        <Button
+          outline={true}
+          onClick={connectWallet}
+          gradientDuoTone="purpleToBlue"
+        >
+          {isConnected ? account : "Connect wallet"}
         </Button>
       </div>
 
